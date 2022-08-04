566b748e924e6f65dab15766fbc14be692c4c1c7
Charlie <ickyicky@gmail.com>
2022-08-04 16:12:34 +0900

    리팩터링: 액션, 계산 분리해보기

--- main.js
+++ main.js
@@ -1,38 +1,77 @@
-let shippingCart = [];
-let shippingCartTotal = 0;
+// 테스트 하기 쉽게 만들기
+// 1. DOM 업데이트와 비즈니스 규칙은 분리되어야 합니다.
+// 2. 전역변수가 없어야 합니다.
 
+// 재사용하기 쉽게 만들기
+// 1. 전역변수에 의존하지 않아야 합니다.
+// 2. DOM을 사용할 수 있는 곳에서 실행된다고 가정하면 안됩니다.
+// 3. 함수가 결괏값을 리턴해야 합니다.
+
+// 액션에서 계산 빼내기
+
+let shippingCart = []; // 액션 변경 가능한 전역변수
+let shippingCartTotal = 0; // 액션 변경 가능한 전역변수
+
+// 액션
 function addItemToCart(name, price) {
-  shippingCart.push({
-    name,
-    price,
-  });
+  shippingCart = addItem(shippingCart, name, price); // 전역변수 수정 액션
+
   calcCartTotal();
 }
 
+// 액션
 function calcCartTotal() {
-  shippingCartTotal = 0;
-  for (let i = 0; i < shippingCart.length; i++) {
-    let item = shippingCart[i];
-    shippingCartTotal += item.price;
-  }
+  shippingCartTotal = calcTotal(shippingCart); // 전역변수 수정 액션
+
   setCartTotalDom();
+
   updateShippingIcons();
+
   updateTaxDom();
 }
 
+// 액션
 function updateShippingIcons() {
-  let buyButtons = getBuyButtonsDom();
-  for(let i = 0; i < buyButtons.length; i ++) {
-    let button = buyButtons[i];
+  let buyButtons = getBuyButtonsDom(); // 액션 DOM에서 읽어옴
+
+  for (const button of buyButtons) {
     let item = button.item;
-    if (item.price + shippingCartTotal >= 20) {
-      button.showFreeShuppingIcon();
+
+    if (isFreeShipping(item.price, shippingCartTotal)) {
+      button.showFreeShippingIcon(); // 액션 DOM 변경
     } else {
-      button.hideFreeShuppingIcon();
+      button.hideFreeShippingIcon();
     }
   }
 }
 
+// 액션
 function updateTaxDom() {
-  setTaxDom(shippingCartTotal * 0.10);
+  setTaxDom(calcTax(shippingCartTotal));
+}
+
+// 계산
+function addItem(cart, name, price) {
+  return [...cart, { name, price }];
+}
+
+// 계산
+function calcTotal(cart) {
+  let total = 0;
+
+  for (const item of cart) {
+    total += item.price;
+  }
+
+  return total;
+}
+
+// 계산
+function isFreeShipping(itemPrice, total) {
+  return itemPrice + total >= 20;
+}
+
+// 계산
+function calcTax(amount) {
+  return amount * 0.1;
 }
