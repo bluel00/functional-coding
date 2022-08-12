6d3961754391e98a7dc953087a4eede93a5b570f
Charlie <ickyicky@gmail.com>
2022-08-12 14:05:56 +0900

    리팩터링

--- week3/statement.js
+++ week3/statement.js
@@ -1,61 +1,92 @@
-function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
-
-  const format = new Intl.NumberFormat('en-US', {
+const format = (number) =>
+  new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format;
+  }).format(number);
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+const printBill = (totalAmount, volumeCredits, customer, detail) => `
+    청구내역 (고객명: ${customer})
+    ${detail}
+    총액: ${format(totalAmount / 100)}
+    적립 포인트: ${volumeCredits}점
+  `;
 
-    let thisAmount = 0;
+const makeBillItemDetail = (plays, performances) => {
+  return performances.reduce((accumulator, { playID, audience }) => {
+    const play = getPlayById(plays, playID);
 
-    switch (play.type) {
-      case 'tragedy': // 비극
-        thisAmount = 40000;
+    const amount = calculateAmount(play.type, audience);
 
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        thisAmount = 30000;
+    return (
+      accumulator + `${play.name} : ${format(amount / 100)} (${audience}석)\n`
+    );
+  }, '');
+};
 
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
+const calculateVolumeCredits = (plays, performances) => {
+  return performances.reduce((accumulator, { playID, audience }) => {
+    const extraPoint = isComedy(plays, playID)
+      ? Math.floor(perf.audience / 5)
+      : 0;
 
-        break;
+    return accumulator + Math.max(audience - 30, 0) + extraPoint;
+  }, 0);
+};
 
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
+const isComedy = (plays, playID) => plays[playID].type === 'comedy';
 
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
+const calculateComedyAmount = (audience) => {
+  const defaultAmount = 30000;
 
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
+  const extraAmount = audience > 20 ? 10000 + 500 * (audience - 20) : 0;
 
-    result += `${play.name} : ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
-  }
+  return defaultAmount + extraAmount + 300 * audience;
+};
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+const calculateTragedyAmount = (audience) => {
+  const defaultAmount = 40000;
 
-  return result;
-}
+  const extraAmount = audience > 30 ? 1000 * (audience - 30) : 0;
+
+  return defaultAmount + extraAmount;
+};
+
+const calculateAmount = (type, audience) => {
+  return {
+    tragedy: calculateTragedyAmount(audience),
+    comedy: calculateComedyAmount(audience),
+  }[type];
+
+  // TODO: error handling 여기서 하는게 맞나?
+  // default:
+  //   throw new Error(`알 수 없는 장르 : ${play.type}`);
+};
+
+const getPlayById = (plays, id) => arrayGet(plays, id);
+
+const arrayGet = (array, index) => array[index];
+
+const calculateTotalAmount = (plays, performances) => {
+  return performances.reduce((accumulator, { playID, audience }) => {
+    const play = getPlayById(plays, playID);
+
+    const amount = calculateAmount(play.type, audience);
+
+    return accumulator + amount;
+  }, 0);
+};
+
+const statement = (invoice, plays) => {
+  const totalAmount = calculateTotalAmount(plays, invoice.performances);
+
+  const volumeCredits = calculateVolumeCredits(plays, invoice.performances);
+
+  const detail = makeBillItemDetail(plays, invoice.performances);
+
+  return printBill(totalAmount, volumeCredits, invoice.customer, detail);
+};
 
 module.exports = {
   statement,
-}
+};
